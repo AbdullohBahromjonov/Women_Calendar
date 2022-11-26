57f9cb5a4a921f93ca6a39528ddde1094c4ee2fd
Abdulloh <abdullohbahromjonv@gmail.com>
2022-11-26 13:22:12 +0500

    Initial Commit

---
+++ Women_Calendar/Women_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Women_CalendarApp.swift
+//  Women_Calendar
+//
+//  Created by Abdulloh on 26/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Women_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
